6600f7f054fabb923816265421d752cf8a91c38f
Akio <rnanimo@gmail.com>
2023-08-12 03:04:25 +0300

    implement AuctionPessimistic using ReentrantLock

--- src/main/java/course/concurrency/exams/auction/AuctionPessimistic.java
+++ src/main/java/course/concurrency/exams/auction/AuctionPessimistic.java
@@ -1,6 +1,6 @@
 package course.concurrency.exams.auction;
 
-import java.util.concurrent.locks.StampedLock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class AuctionPessimistic implements Auction {
 
@@ -10,7 +10,7 @@ public class AuctionPessimistic implements Auction {
         this.notifier = notifier;
     }
 
-    private final StampedLock stampedLock = new StampedLock();
+    private final ReentrantLock lock = new ReentrantLock();
     private volatile Bid latestBid;
 
     /**
@@ -20,50 +20,26 @@ public class AuctionPessimistic implements Auction {
      * @return true if the latest bid was updated, false otherwise
      */
     public boolean propose(Bid bid) {
-        long stamp = stampedLock.tryOptimisticRead();
-
-        if (latestBid == null || isNewBidHigher(bid)) {
-            if (stampedLock.validate(stamp)) { // checking if the latest bid was updated by another thread
-                updateLatestBid(bid);
-                return true;
-            } else {
-                return updateLatestBidIfNeeded(bid);
+        if (shouldBidBeUpdated(bid)) {
+            try {
+                lock.lock();
+                if (shouldBidBeUpdated(bid)) {
+                    notifier.sendOutdatedMessage(latestBid);
+                    latestBid = bid;
+                    return true;
+                }
+            } finally {
+                lock.unlock();
             }
         }
         return false;
     }
 
     public Bid getLatestBid() {
-        long stamp = stampedLock.tryOptimisticRead();
-        Bid lastAcceptedBid = latestBid;
-        if (!stampedLock.validate(stamp)) {
-            stamp = stampedLock.readLock();
-            lastAcceptedBid = latestBid;
-            stampedLock.unlockRead(stamp);
-        }
-        return lastAcceptedBid;
+        return latestBid;
     }
 
-    private boolean isNewBidHigher(Bid bid) {
-        return bid.getPrice() > latestBid.getPrice();
-    }
-
-    private void updateLatestBid(Bid bid) {
-        long stamp = stampedLock.writeLock();
-        notifier.sendOutdatedMessage(latestBid);
-        latestBid = bid;
-        stampedLock.unlockWrite(stamp);
-    }
-
-    private boolean updateLatestBidIfNeeded(Bid bid) {
-        boolean isBidUpdated = false;
-        long stamp = stampedLock.writeLock();
-        if (isNewBidHigher(bid)) {
-            notifier.sendOutdatedMessage(latestBid);
-            latestBid = bid;
-            isBidUpdated = true;
-        }
-        stampedLock.unlockWrite(stamp);
-        return isBidUpdated;
+    private boolean shouldBidBeUpdated(Bid bid) {
+        return latestBid == null || bid.getPrice() > latestBid.getPrice();
     }
 }
